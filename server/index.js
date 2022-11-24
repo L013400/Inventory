a25d4a458d39eb6802d089266237cabb773d48c3
Anurag <dwivedi_anurag@network.lilly.com>
2022-11-24 18:20:23 +0530

    Index file update

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import apiRouter from './routes/index.js'
 import dotenv from "dotenv"
+import path from "path"
+import { fileURLToPath } from 'url'
 
 dotenv.config()
 const app = new express();
@@ -11,3 +13,16 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log(`Server started on PORT ${PORT}`)
 })
+
+const IS_PROD = process.env.ENVIRONMENT === "prod" ? true : false
+if(IS_PROD){
+    const _filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);
+const buildPath = path.join(_dirname,'../client/build')
+app.use(express.static(buildPath))
+
+app.get('/',(req,res)=>{
+    res.sendFile(buildPath +'/index.html')
+})
+    
+}
